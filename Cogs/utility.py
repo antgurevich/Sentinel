a25b49e6192ff544ca894fc32d9c6db58eb91c78
Anton Gurevich <71106751+antgurevich@users.noreply.github.com>
2020-12-27 16:39:23 -0600

    Added .s weather

--- Cogs/utility.py
+++ Cogs/utility.py
@@ -1,3 +1,6 @@
+import requests
+import json
+from configparser import ConfigParser
 import discord
 from discord.ext import commands
 
@@ -5,7 +8,44 @@ class Utility(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
 ###########################################################################
-    @commands.command(name="temp", aliases=["convert","tempconvert","tconvert","tcon"])
+    @commands.command(name="weather")
+    async def weather(self, ctx, city):
+        try:
+            API_KEY=os.environ("WEATHER_API_KEY")
+        except:
+            config_object=ConfigParser()
+            config_object.read("SentinelVariables.ini")
+            variables=config_object["variables"]
+            API_KEY=variables["WEATHER_API_KEY"]
+        
+        baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
+        fullUrl = baseUrl + "appid=" + API_KEY + "&q=" + city
+        response = requests.get(fullUrl) 
+
+        responses = response.json() 
+
+        if responses["cod"] == "404":
+            await ctx.send("City not found! Be sure to correctly spell it")
+        else:
+            main = responses["main"] 
+            currentTemperature = main["temp"] 
+            tempC=round(currentTemperature-273.15,2) #Converts to celsius
+            tempF=round(((9.0/5.0 * tempC) + 32),2)
+            #currentPressure = main["pressure"] 
+            #currentHumidiy = main["humidity"] 
+            weather = responses["weather"] 
+            weather_description = weather[0]["description"] 
+
+            embed=discord.Embed(title=("Current Weather in "+city), color=discord.Color.blue())
+            embed.add_field(name="Temperature", value=(str(tempC)+"°C / "+str(tempF)+"°F"))
+            embed.add_field(name="Weather",value=weather_description)
+            await ctx.send(embed=embed)
+    @weather.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("You need to include the city you want to check! .s weather [city]")
+###########################################################################
+    @commands.command(name="temp", aliases=["tempconvert","tconvert","tcon"])
     async def tempconvert(self, ctx, temp, unit):
         
         try:
