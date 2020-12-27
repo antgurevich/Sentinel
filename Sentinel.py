cf3ff3159a9e76a5eb5175835ca0fec4f3dcfa73
Anton Gurevich <71106751+antgurevich@users.noreply.github.com>
2020-12-26 20:44:15 -0600

    Fixed on_member_remove gif sending to wrong channel, changed cogList to .json

--- Sentinel.py
+++ Sentinel.py
@@ -1,18 +1,22 @@
+import psycopg2
 import json
 import os
 import discord
 from discord.ext import commands
 
 bot=commands.Bot(command_prefix=commands.when_mentioned_or(".s ")
-                ,description="Test Description"
                 ,case_insensative=True
+                ,owner_id=338707493188272129
                 ,intents=discord.Intents().all())
 bot.remove_command("help")
 ###########################################################################
 @bot.event #Loads all cogs and initiates bot
 async def on_ready():
     
-    cogList=["fun","misc","games", "mod"]
+    with open("SentinelHelp.json","r") as cogFile:
+        data=json.load(cogFile)
+    data=data["Cogs"]
+    cogList=list(data.keys())
     
     for cog in cogList:
         cog=("Cogs."+cog)
@@ -21,7 +25,7 @@ async def on_ready():
             print ("Loaded",cog)
         except Exception as e:
             print ("Error loading",cog,"e:",e)
-    
+
     await bot.change_presence(activity=discord.Game(name="with Cache's emotions"))
     print (bot.user.name,"successfully connected to Discord")
 ###########################################################################
@@ -49,15 +53,15 @@ async def on_member_remove(member):
     for channel in member.guild.text_channels:
         if isinstance(channel, discord.TextChannel):
             if "goodbye" in str(channel):
-                await channel.send("Adios "+member.mention+"... \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t**asshole**")
+                await channel.send("Adios "+member.mention+"... \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t")
                 embed.set_image(url="https://media.giphy.com/media/ef0ZKzcEPOBhK/giphy.gif")
                 await channel.send(embed=embed)
                 return
     sysChannel=member.guild.system_channel
     if sysChannel:
-        await sysChannel.send("Adios "+member.mention+"... \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t**asshole**")
+        await sysChannel.send("Adios "+member.mention+"... \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t")
         embed.set_image(url="https://media.giphy.com/media/ef0ZKzcEPOBhK/giphy.gif")
-        await channel.send(embed=embed)
+        await sysChannel.send(embed=embed)
 ###########################################################################
 @bot.command(name="help") #Help messages
 async def help(ctx, arg: str=""):
@@ -100,16 +104,6 @@ async def help(ctx, arg: str=""):
 async def clear_error(ctx,error):
     if isinstance(error,commands.MissingRequiredArgument):
         await ctx.send(ctx.message.author.mention+", guess what? You typed it wrong *dipshit*, use *.s help -f* for an extensive guide or *.s help -s* for a short guide")
-###########################################################################
-'''@bot.event
-async def on_message(message):
-    if message.author==bot.user: #Ensures bot doesn't respond to itself
-        return
-    
-    if message.content.startswith("hi"):
-        await message.channel.send("Hello @"+str(message.author.mention))
-
-    await bot.process_commands(message) #Enables commands'''
 ###########################################################################
 try:
     bot.run(os.environ["DISCORDTOKEN"])
