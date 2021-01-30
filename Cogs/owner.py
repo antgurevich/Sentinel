ace9eb91ae25df16435cf5511bafd3715bd42846
Anton Gurevich <71106751+antgurevich@users.noreply.github.com>
2021-01-29 20:01:01 -0600

    Disabled cog listener to fix it

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -16,13 +16,13 @@ class Owner(commands.Cog):
     async def clear_error(self, ctx, error):
         if isinstance(error,commands.NotOwner):
             await ctx.send("You're not the owner dummy")
-    
+    '''
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.guild is None and not message.author.bot:
             user = self.bot.get_user(338707493188272129)
             await user.send(f"**{message.author} sent:** {message.content}")
-        await self.bot.process_commands(message)
+        await self.bot.process_commands(message)'''
 ###########################################################################
 def setup(bot):
     bot.add_cog(Owner(bot))
