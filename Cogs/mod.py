02b502ced273af8a35cf06b936796fbd4b5a4a33
Anton Gurevich <71106751+antgurevich@users.noreply.github.com>
2021-02-23 18:10:53 -0600

    Added db closing/opening, some commands, made time required

--- Cogs/mod.py
+++ Cogs/mod.py
@@ -1,9 +1,8 @@
 import asyncio
-import psycopg2
-import os
-from configparser import ConfigParser
 import discord
 from discord.ext import commands
+
+from Cogs.db import dbconnect
 
 class Mod(commands.Cog):
     def __init__(self,bot):
@@ -57,9 +56,10 @@ class Mod(commands.Cog):
     @commands.command(name="warnlist",aliases=["listwarns"])
     @commands.has_permissions(kick_members=True)
     async def warnlist(self, ctx, user: discord.Member):
+        cursor,conn=dbconnect() #Opens connection to db
         try:
-            sql=('''SELECT row_id, reason FROM warning_history WHERE (guild_id=%s AND user_id=%s);''')
-            cursor.execute(sql, (ctx.guild.id, user.id))
+            sql=('''SELECT row_id, reason FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND type=%s);''')
+            cursor.execute(sql, (ctx.guild.id, user.id, "warn"))
             results=cursor.fetchall()
         except Exception as e:
             print (e)
@@ -72,13 +72,12 @@ class Mod(commands.Cog):
             embed.add_field(name=f"{x+1})  Warn ID: {results[x][0]}",value=f"Reason: {results[x][1]}")
             x+=1
         await ctx.send(embed=embed)
+        conn.close() #Closes connection to db
 
     @warnlist.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="You do not have the `Kick Members` permission!"))
-        elif isinstance(error, commands.UserNotFound):
-            await ctx.send(embed=discord.Embed(title="Could not find this user! Make sure to ping them correctly"))
         elif isinstance(error, commands.BadArgument):
             await ctx.send(embed=discord.Embed(title="Incorrect argument type! Use `.s warnlist (@user)`"))
         elif isinstance(error, commands.MissingRequiredArgument):
@@ -87,28 +86,28 @@ class Mod(commands.Cog):
     @commands.command(name="unwarn", aliases=["clearwarn","delwarn","removewarn","deletewarn"])
     @commands.has_permissions(kick_members=True)
     async def unwarn(self, ctx, user: discord.Member, id: int):
+        cursor,conn=dbconnect() #Opens connection to db
         try:
-            sql=('''SELECT * FROM warning_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
+            sql=('''SELECT * FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
             cursor.execute(sql, (ctx.guild.id, user.id,id))
             results=cursor.fetchone()
             if results is None:
                 await ctx.send(embed=discord.Embed(title="Warning ID not found! Use `.s warnlist (@user)` to select the right ID!"))
                 return
                 
-            sql=('''DELETE FROM warning_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
+            sql=('''DELETE FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
             cursor.execute(sql, (ctx.guild.id, user.id,id))
             conn.commit
            
         except Exception as e:
             print (e)
         await ctx.send(embed=discord.Embed(title="Warning successfully deleted!"))
+        conn.close() #Closes connection to db
 
     @unwarn.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="You do not have the `Kick Members` permission!"))
-        elif isinstance(error, commands.UserNotFound):
-            await ctx.send(embed=discord.Embed(title="Could not find this user! Make sure to ping them correctly"))
         elif isinstance(error, commands.BadArgument):
             await ctx.send(embed=discord.Embed(title="Incorrect argument type! Use `.s unwarn (@user) (warn ID)`"))
         elif isinstance(error, commands.MissingRequiredArgument):
@@ -116,7 +115,8 @@ class Mod(commands.Cog):
 ###########################################################################
     @commands.command(name="warn")
     @commands.has_permissions(kick_members=True)
-    async def warn(self, ctx, user: discord.Member, *, reason=None):
+    async def warn(self, ctx, user: discord.Member, *, reason=None):     
+        cursor,conn=dbconnect() #Opens connection to db
         exceptions=[805830355290161153]#Smth
         if (ctx.author.top_role.position <= user.top_role.position) and (ctx.guild.owner.id != ctx.author.id) and (user.top_role.id not in exceptions):
             await ctx.send(embed=discord.Embed(title="You are trying to warn someone who has a role higher or equal to yours! Smh my head"))
@@ -128,11 +128,15 @@ class Mod(commands.Cog):
             else:
                 await user.send(embed=discord.Embed(title=f"You have been warned in **{ctx.guild}**"))
                 embed.add_field(name="Warning", value=(f"**User **{user.mention} has been warned by {ctx.author.mention}"))
-            sql=('''INSERT INTO warning_history(guild_id, user_id, reason)
-                    VALUES (%s, %s, %s)''')
-            cursor.execute(sql, (ctx.guild.id,user.id, reason))
-            conn.commit()
-            await ctx.send(embed=embed)
+            try:
+                sql=('''INSERT INTO moderator_history(guild_id, type, user_id, username, reason)
+                    VALUES (%s, %s, %s, %s, %s)''')
+                cursor.execute(sql, (ctx.guild.id, "warn", user.id, str(user), reason))
+                conn.commit()
+                await ctx.send(embed=embed)
+            except Exception as e:
+                print (e)
+        conn.close() #Closes connection to db
     
     @warn.error
     async def clear_error(self, ctx, error):
@@ -145,10 +149,12 @@ class Mod(commands.Cog):
 ###########################################################################
     @commands.command(name="mute")
     @commands.has_permissions(kick_members=True)
-    async def mute(self,ctx, user: discord.Member, time: str = None, *, reason):
+    async def mute(self,ctx, user: discord.Member, time: str, *, reason):    
+        cursor,conn=dbconnect() #Opens connection to db        
         exceptions=[805830355290161153]#Smth
         if (ctx.author.top_role.position <= user.top_role.position) and (ctx.guild.owner.id != ctx.author.id) and (user.top_role.id not in exceptions):
             await ctx.send(embed=discord.Embed(title="You are trying to mute someone who has a role higher or equal to yours! Smh my head",color=discord.Color.red()))
+            return
         else:
             guild=ctx.guild
             muteRole=None
@@ -167,11 +173,14 @@ class Mod(commands.Cog):
                     muteRole=await guild.create_role(name="Muted",color=discord.Color.dark_grey(),permissions=perms)
                     for channel in guild.channels:
                         await channel.set_permissions(muteRole,send_messages=False)
-                 
-                if time is None: #Permamuted
+                sql=('''INSERT INTO moderator_history(guild_id, type, user_id, username, duration, reason)
+                    VALUES (%s, %s, %s, %s, %s, %s)''')
+                cursor.execute(sql, (ctx.guild.id, "mute", user.id, str(user), time, reason))
+                conn.commit()
+                if time=="perm": #Permamuted
                     await user.add_roles(muteRole)
                     embed=discord.Embed()
-                    embed.add_field(name="You guys wanna see a joke?",value=f"{user.mention} getting muted for *{reason}*")
+                    embed.add_field(name="You guys wanna see a joke?",value=f"{user.mention} getting muted for **{reason}**")
                     await ctx.send(embed=embed)
                 
                 else: #User specified time limit
@@ -193,7 +202,7 @@ class Mod(commands.Cog):
                     await user.add_roles(muteRole)
                     embed=discord.Embed()
                     
-                    embed.add_field(name="You guys wanna see a joke?",value=f"{user.mention} getting silenced for {parsedTime} {timeUnit} for *{reason}*, what an L")
+                    embed.add_field(name="You guys wanna see a joke?",value=f"{user.mention} getting silenced for {parsedTime} {timeUnit} for **{reason}**, what an L")
                     
                     await ctx.send(embed=embed)
 
@@ -207,15 +216,16 @@ class Mod(commands.Cog):
                     if muteRole in user.roles:
                         await user.remove_roles(muteRole)
                         await ctx.send(embed=discord.Embed(title=f"{user} has been unmuted after {parsedTime} {timeUnit}!",color=discord.Color.red()))
-
+            conn.close() #Closes connection to db
+    
     @mute.error
     async def clear_error(self,ctx,error):
         if isinstance(error,commands.MissingPermissions):
             await ctx.send(embed=discord.Embed(title="You can't mute people dipshit, you don't have permission"))
         elif isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send(embed=discord.Embed(title="You missed a parameter :rolling_eyes:\nUse `.s mute (@username) [time] (reason)`\nExample: `.s mute @test_user 5m spam`"))
+            await ctx.send(embed=discord.Embed(title="You missed a parameter :rolling_eyes:\nUse `.s mute (@username) (time) (reason)`\nExample: `.s mute @test_user 5m spam` or `.s mute @test_user perm nsfw`"))
         elif isinstance(error, commands.BadArgument) or isinstance(error, commands.UserNotFound):
-            await ctx.send(embed=discord.Embed(title="Invalid user! Use `.s mute (@username) [time] (reason)`"))
+            await ctx.send(embed=discord.Embed(title="Invalid user! Use `.s mute (@username) (time) (reason)`"))
 ###########################################################################
     @commands.command(name="unmute")
     @commands.has_permissions(kick_members=True)
@@ -246,11 +256,70 @@ class Mod(commands.Cog):
             await ctx.send(embed=discord.Embed(title="Invalid user! Use `.s unmute (@username)`"))
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(embed=discord.Embed(title="Who am I unmuting? You gotta tell me who, use `.s unmute @username`"))
+###########################################################################
+    @commands.command(name="mutelist", aliases=["listmutes"])
+    @commands.has_permissions(kick_members=True)
+    async def mutelist(self, ctx, user: discord.Member):
+        cursor,conn=dbconnect() #Opens connection to db
+        try:
+            sql=('''SELECT row_id, duration, reason FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND type=%s);''')
+            cursor.execute(sql, (ctx.guild.id, user.id, "mute"))
+            results=cursor.fetchall()
+        except Exception as e:
+            print (e)
+        if len(results)==0:
+            await ctx.send(embed=discord.Embed(title="This user has no history of mutes!"))
+            return
+        embed=discord.Embed(title=f"{user}'s Mute History:",color=discord.Color.dark_blue())
+        x=0
+        for mute in results:
+            embed.add_field(name=f"{x+1})  Mute ID: {results[x][0]}",value=f"Reason: {results[x][2]} --- Duration: {results[x][1]}")
+            x+=1
+        await ctx.send(embed=embed)
+        conn.close() #Closes connection to db
 
+    @mutelist.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(embed=discord.Embed(title="You do not have the `Kick Members` permission!"))
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(embed=discord.Embed(title="Incorrect argument type! Use `.s mutelist (@user)`"))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=discord.Embed(title="Enter the person you want to list the warns of! Use `.s mutelist (@user)`"))
+###########################################################################
+    @commands.command(name="clearmute")
+    @commands.has_permissions(kick_members=True)
+    async def clearmute(self, ctx, user: discord.Member, id: int):       
+        cursor,conn=dbconnect() #Opens connection to db      
+        try:
+            sql=('''SELECT * FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
+            cursor.execute(sql, (ctx.guild.id, user.id,id))
+            results=cursor.fetchone()
+            if results is None:
+                await ctx.send(embed=discord.Embed(title="Mute ID not found! Use `.s mutelist (@user)` to select the right ID!"))
+                return
+                
+            sql=('''DELETE FROM moderator_history WHERE (guild_id=%s AND user_id=%s AND row_id=%s);''')
+            cursor.execute(sql, (ctx.guild.id, user.id,id))
+            conn.commit
+           
+        except Exception as e:
+            print (e)
+        await ctx.send(embed=discord.Embed(title="Mute successfully deleted!"))
+        conn.close() #Closes connection to db
+
+    @clearmute.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send(embed=discord.Embed(title="You do not have the `Kick Members` permission!"))
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(embed=discord.Embed(title="Incorrect argument type! Use `.s clearmute (@user) (mute ID)`"))
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(embed=discord.Embed(title="Enter the person & mute ID you want to clear the mute history from!\nUse `.s mutelist (@user)` to find the IDs, and then `.s clearmute (@user) (mute ID)`"))
 ###########################################################################
     @commands.command(name="kick")
     @commands.has_permissions(kick_members=True)
-    async def kick(self,ctx, user: discord.Member, *, reason=None):
+    async def kick(self, ctx, user: discord.Member, *, reason=None):
         if (ctx.author.top_role.position <= user.top_role.position) and (ctx.guild.owner.id != ctx.author.id):
             await ctx.send(embed=discord.Embed(title="You are trying to kick someone who has a role higher or equal to yours! Smh my head"))
         else:
@@ -319,17 +388,4 @@ class Mod(commands.Cog):
             await ctx.send(embed=discord.Embed(title="You did it wrong 4head, use `.s unban username#tags`"))
 ###########################################################################
 def setup(bot):
-    global cursor, conn
-    try:
-        conn=psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
-        cursor = conn.cursor()
-        print ("Mod Cog: Database connection established from environment")
-    except:
-        config_object=ConfigParser()
-        config_object.read("SentinelVariables.ini")
-        variables=config_object["variables"]
-        DATABASE_URL=variables["DATABASE_URL"]
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cursor = conn.cursor()
-        print ("Mod Cog: Database connection established from .ini")
     bot.add_cog(Mod(bot))
