7c29b2c0f72b1ced00d37a4816b5a723ed05a184
Anton Gurevich <antgurevich@gmail.com>
2022-09-13 00:02:37 -0500

    Added shutdown message

--- Cogs/owner.py
+++ Cogs/owner.py
@@ -71,6 +71,26 @@ class Owner(commands.Cog):
             channel=self.bot.get_channel(784243375783149568)
             await channel.send(embed=discord.Embed(title=f"Error occured while clearing 'yoink_commands' table: {e}"))
 ###########################################################################
+    @commands.command(name="shutdownmessage")
+    @commands.is_owner()
+    async def shutdownmessage(self, ctx):
+        botguilds=list(self.bot.guilds)
+        shutdown = "Hey everyone! Sentinel will be shutting down November 20th, 2022 due to the servers it is hosted on ending their free tier subscriptions. \nThere are currently no plans to continue hosting later on, but this might change.\nThank you for an amazing two years of support (:"
+        #botguilds = [783840649282846731]
+        print(botguilds)
+        # for guild in botguilds:
+        #     print(guild.id)
+        for guild in botguilds:
+            guild = self.bot.get_guild(guild.id)
+            sysChannel=guild.system_channel
+            if sysChannel:
+                await sysChannel.send(embed = discord.Embed(title = 'Sentinel Shutdown', description = shutdown, color = discord.Color.blue()))
+
+    
+    @shutdownmessage.error
+    async def clear_error(self, ctx, error):
+        print(f"Error occured during sending message to all servers; Error: {error}")
+###########################################################################
     @commands.command(name="sendmessage",aliases=["sendmsg","senddm","sendpm"])
     @commands.is_owner()
     async def sendmessage(self, ctx, id, *, message):
